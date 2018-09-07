7f9e003c25cf61be944cb10281f537bdae8b872e
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-09-06 18:58:12 -0700

    Add ViewModel for Cat Contract without data validation

---
+++ PDFWriter/PDFWriter/ViewModels/CatContractViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PDFWriter.ViewModels
+{
+    public class CatContractViewModel
+    {
+        [Required]
+        public string OwnerName1 { get; set; }
+        public string CatName { get; set; }
+        public string Name2 { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string OwnerName2 { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public LunarState State { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Email { get; set; }
+        public string CatName2 { get; set; }
+        public float Weight { get; set; }
+        public string Color { get; set; }
+        public bool Fixed { get; set; }
+        public int ChipNumber { get; set; }
+        public string VetName { get; set; }
+        public string VetAddress { get; set; }
+        public string VetPhone { get; set; }
+    }
+    public enum LunarState
+    {
+        Crateriton,
+        MoonRock,
+        Farside,
+        Lunida,
+        Terran,
+        Crescent,
+        Eclipse,
+        Solaris,
+        Nebulon,
+        Kepler,
+        Daedalus,
+        Nectaris,
+        Imbrium,
+        Gibbous,
+        Sinus,,
+        Breccia
+        MilkyWay
+    }
+}
