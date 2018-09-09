c4a8b6f2e5bc3a53c1bb6fbcf3fce70adb69911a
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-09-09 12:44:57 -0700

    Update Home Controller to work with creategrd, relocate source files and delete source files in solution

--- PDFWriter/PDFWriter/Controllers/HomeController.cs
+++ PDFWriter/PDFWriter/Controllers/HomeController.cs
@@ -21,74 +21,84 @@ namespace GoogleDrive.Controllers
     {
 
         // GET: Home
-        
-
-            //Get: Cat COntract
-            public ActionResult CatContract(CatContractViewModel vm)
+        public ActionResult CatContract()
         {
-            PrintViewModelToPDF(vm);
-            return View("Success");
-        }
-        //Get: Cat COntract
-        public ActionResult SignContract(CatContractViewModel vm)
-        {
-            //PrintViewModelToPDF(vm);
             return View();
         }
-        public void PrintViewModelToPDF(dynamic vm)
+        //POST: Cat Contract Data and save
+        [HttpPost]
+        public ActionResult CatContract(CatContractViewModel vm)
+        {
+            //Where to save the completed document
+            string savePath = @"C:/temp/";
+            //Where to get the PDF to print on
+            string sourcePath = @"C:/temp/Cat Contract.pdf";
+            //JSON coordinates of where to print on the new PDF
+            string JSONPath = @"C:/temp/Cat Contract Coordinates.json";
+
+            //Create the document and save it to the specified location
+            string agreementLocation = PrintViewModel(vm, savePath, sourcePath, JSONPath);
+
+            //Email the document to registration
+            //EmailMethod()
+
+            return View();
+        }
+
+        public string PrintViewModel(dynamic vm, string savePath, string sourcePath, string JSONPath)
         {
             /* Load PDF original into a new PDFDocument so that it can be printed to*/
-            //Where to save the completed document
-            string targetPath = @"C:/temp/Catcontract.pdf";
-            //Where to get the PDF to print on
-            string sourcePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"AppData\CatContract.pdf");
 
             PdfDocument originalPDF = PdfReader.Open(sourcePath, PdfDocumentOpenMode.Import);
             PdfDocument newPDF = new PdfDocument();
             newPDF.Info.Author = "Author";
-            newPDF.Info.Keywords = "Cat Contract";
+            newPDF.Info.Keywords = "Enrollment";
             newPDF.Info.Title = "Document Title";
             for (int p = 0; p < originalPDF.PageCount; p++)
             {
                 PdfPage page = newPDF.AddPage(originalPDF.Pages[p]);
                 page.Size = PageSize.A4;
             }
-
-
             //Load vm properties so that they can be looped through
             Type type = vm.GetType();
             PropertyInfo[] VMProperties = type.GetProperties();
 
             //Load JSON Data for printing values and create dynamic variable to hold values
-            string JSONPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"AppData\CatContract.json");
             JObject jsonPageFields = JObject.Parse(System.IO.File.ReadAllText(JSONPath)) as JObject;
-            dynamic pages = jsonPageFields.First;
+            dynamic pages = jsonPageFields.Values();
 
             /*Loop through JSON Data and look for key values with corresponding Class properties and if
              * found, get the values from the ViewModel and JSON data send to print method*/
 
             foreach (PropertyInfo prop in VMProperties)
             {
-                foreach (var page in pages)
+                foreach (JToken page in pages)
                 {
-                    foreach (var inputs in page["inputs"])
+                    var pageNumber = Convert.ToInt16(page.Path.Remove(0, 4));
+                    foreach (JObject inputs in page["inputs"])
                     {
-                        if (inputs.GetValue("field") == prop.Name && prop.GetValue(vm) != null)
+                        if (inputs.Value<string>("field") == prop.Name && prop.GetValue(vm) != null)
                         {
-                            double top = (double)inputs.GetValue("top");
-                            double left = (double)inputs.GetValue("left");
-                            string fontFamily = (string)inputs.GetValue("font") != null ? (string)inputs.GetValue("font") : "Arial";//Set Default fault in case none specified
-                            int pageNumber = Convert.ToInt16(pages.Path.Replace("page", ""));
+                            double top = inputs.Value<double>("top");
+                            double left = inputs.Value<double>("left");
+                            string fontFamily = inputs.Value<string>("font") != null ? (string)inputs.Value<string>("font") : "Arial";//Set Default fault in case none specified
                             string propValue = prop.GetValue(vm).ToString();
                             PrintElement(top, left, fontFamily, pageNumber, propValue);
                         }
                     }
-
                 }
 
             }
-            newPDF.Save(targetPath);
-            Process.Start(targetPath);
+            //Build savePath with applicant's name
+            savePath += "CatContract-" + vm.Name.Replace(" ", "") + DateTime.Today.ToString("MMddyyyy") + ".pdf";
+
+
+            //Add try catch in case location already exists
+            newPDF.Save(savePath);
+            Process.Start(savePath);
+
+            return savePath;
+
             void PrintElement(double top, double left, string fontFamily, int pageNumber, string textToPrint)
             {
                 XGraphics gfx = XGraphics.FromPdfPage(newPDF.Pages[pageNumber - 1]);
@@ -100,74 +110,94 @@ namespace GoogleDrive.Controllers
             }
 
         }
-
+        //GET CreateGrid
+        public ActionResult CreateGrid()
+        {
+            return View();
+        }
 
         /*Use createGrid to print the target document with a grid of numbers so that input box locations can be determined
          * Grids can be a single vertical or horizontal line or a full page grid
          * Change the locations in the function call to change you filepaths and uncomment the appropriate loop sections*/
-        public void createGrid(string source, string target)
+         //POST CreateGrid
+         [HttpPost]
+        public ActionResult createGrid()
         {
-            PdfDocument originalPDF = PdfReader.Open(source, PdfDocumentOpenMode.Import);
-            PdfDocument newPDF = new PdfDocument();
-            newPDF.Info.Author = "Author";
-            newPDF.Info.Keywords = "Enrollment";
-            newPDF.Info.Title = "Document Title";
-            for (int p = 0; p < originalPDF.PageCount; p++)
+            string path = "c:/temp/";
+            string file = @"Cat Contract.pdf";
+
+            try
             {
-                PdfPage page = newPDF.AddPage(originalPDF.Pages[p]);
-                page.Size = PageSize.A4;
-            }
-
-            /*Loop 1: Create one vertical line of coordinates. Change left to move line horizontally. Coordinates in (left,top) format*/
-            //double left = 260;
-            //for (int top = 0; top < 36; top++)
-            //{
-            //    using (XGraphics gfx = XGraphics.FromPdfPage(page))
-            //    {
-            //        //XGraphics gfx = XGraphics.FromPdfPage(page);
-            //        XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
-            //        XPoint xTL = new XPoint(left, top * 25);
-            //        XPoint xBR = new XPoint(left, top * 25);
-            //        gfx.DrawString((left).ToString() + "," + (top * 25).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
-            //    }
-            //}
-
-            /*Loop 2: Create one horizontal line of coordinates. Change top to move line vertically. Coordinates in (left,top) format*/
-            //double top = 450;
-            //for (int left = 0; left < 25; left++)
-            //{
-            //    using (XGraphics gfx = XGraphics.FromPdfPage(page))
-            //    {
-            //        //XGraphics gfx = XGraphics.FromPdfPage(page);
-            //        XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
-            //        XPoint xTL = new XPoint(left *25, top);
-            //        XPoint xBR = new XPoint(left *25, top);
-            //        gfx.DrawString((left * 25).ToString() + "," + (top).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
-            //    }
-            //}
-
-            //Loop 3: Create entire page of gridding
-            for (int p = 0; p < newPDF.PageCount; p++)
-            {
-                for (int x = 0; x < 25; x++)
+                PdfDocument originalPDF = PdfReader.Open(path + file, PdfDocumentOpenMode.Import);
+                PdfDocument newPDF = new PdfDocument();
+                newPDF.Info.Author = "Lunar Animal Control";
+                newPDF.Info.Keywords = "Cat Contract Grid";
+                newPDF.Info.Title = "Cat Contract Grid";
+                for (int p = 0; p < originalPDF.PageCount; p++)
                 {
-                    for (int y = 0; y < 36; y++)
+                    PdfPage page = newPDF.AddPage(originalPDF.Pages[p]);
+                    page.Size = PageSize.A4;
+                }
+
+                /*Loop 1: Create one vertical line of coordinates. Change left to move line horizontally. Coordinates in (left,top) format*/
+                //double left = 260;
+                //for (int top = 0; top < 36; top++)
+                //{
+                //    using (XGraphics gfx = XGraphics.FromPdfPage(page))
+                //    {
+                //        //XGraphics gfx = XGraphics.FromPdfPage(page);
+                //        XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
+                //        XPoint xTL = new XPoint(left, top * 25);
+                //        XPoint xBR = new XPoint(left, top * 25);
+                //        gfx.DrawString((left).ToString() + "," + (top * 25).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
+                //    }
+                //}
+
+                /*Loop 2: Create one horizontal line of coordinates. Change top to move line vertically. Coordinates in (left,top) format*/
+                //double top = 450;
+                //for (int left = 0; left < 25; left++)
+                //{
+                //    using (XGraphics gfx = XGraphics.FromPdfPage(page))
+                //    {
+                //        //XGraphics gfx = XGraphics.FromPdfPage(page);
+                //        XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
+                //        XPoint xTL = new XPoint(left *25, top);
+                //        XPoint xBR = new XPoint(left *25, top);
+                //        gfx.DrawString((left * 25).ToString() + "," + (top).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
+                //    }
+                //}
+
+                //Loop 3: Create entire page of gridding
+                for (int p = 0; p < newPDF.PageCount; p++)
+                {
+                    for (int x = 0; x < 25; x++)
                     {
-                        using (XGraphics gfx = XGraphics.FromPdfPage(newPDF.Pages[p]))
+                        for (int y = 0; y < 36; y++)
                         {
-                            //XGraphics gfx = XGraphics.FromPdfPage(page);
-                            XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
-                            XPoint xTL = new XPoint(x * 25, y * 25);
-                            XPoint xBR = new XPoint(x * 25, y * 25);
-                            gfx.DrawString((x * 25).ToString() + "," + (y * 25).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
+                            using (XGraphics gfx = XGraphics.FromPdfPage(newPDF.Pages[p]))
+                            {
+                                //XGraphics gfx = XGraphics.FromPdfPage(page);
+                                XFont font = new XFont("Times New Roman", 6, XFontStyle.BoldItalic);
+                                XPoint xTL = new XPoint(x * 25, y * 25);
+                                XPoint xBR = new XPoint(x * 25, y * 25);
+                                gfx.DrawString((x * 25).ToString() + "," + (y * 25).ToString(), font, XBrushes.Black, new XRect(xTL, xBR));
+                            }
                         }
                     }
                 }
+                //Save Location for the Grid File
+                string filename = path + "Grid -" + file;
+                newPDF.Save(filename);
+                Process.Start(filename);
+                ViewBag.Message = "Grid Created Succesfully";
+                return View();
             }
-            //Save Location for the Grid File
-            string filename = target;
-            newPDF.Save(filename);
-            Process.Start(filename);
+            catch (Exception)
+            {
+
+                ViewBag.Message = "Something went wrong, plase try again";
+                return View();
+            }
         }
 
 
