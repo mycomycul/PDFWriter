da0a6b8a8007fd7581ec3599b93505835a7f7204
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-03-23 13:10:12 -0700

    Create Credentials class for Emailing

---
+++ PDFWriter/PDFWriter/Models/Credentials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PDFWriter.Models
+{
+    internal class Credentials
+    {
+         internal string Password { get; set; }
+         internal string Email { get; set; }
+         internal string HostAddress { get; set; }
+         internal int HostPort { get; set; }
+
+        internal Credentials(string email, string password, string hostAddress, int hostPort)
+        {
+            Email = email;
+            Password = password;
+            HostAddress = hostAddress;
+            HostPort = hostPort;
+        }
+    }
+}
